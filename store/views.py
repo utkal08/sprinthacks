6dc42d63a607294fbf16d468ec835e3bcf0898cd
ashura <ghanatva@gmail.com>
2023-04-02 10:44:23 +0530

    lala

--- store/views.py
+++ store/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request,'store/index.html')
 
+def contact(request):
+    return render(request,'store/contact.html')
+
+def about(request):
+    return render(request,'store/about.html')
+
