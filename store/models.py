4b53ecd8ee899c7c9c234101e2c42bada3409003
ashura <ghanatva@gmail.com>
2023-04-02 11:05:06 +0530

    models and static files

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    #manufacturer=models.ManyToManyField()
+    price=models.IntegerField()
+    image=models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+class Manufacturer(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    phone=models.BigIntegerField()
+    address=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
